2bda2d0fbfe778f031b2c46fcbf3b973a80d1193
crypto-tobebetter <32189645+tbb-tobebetter@users.noreply.github.com>
2022-07-07 19:03:27 +0800

    Add files via upload

---
+++ SM2-AS-our-test/build/CMakeFiles/test_SM2_AS_our.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_SM2_AS_our.dir/test/test_SM2_AS_our.cpp.o"
+  "test_SM2_AS_our"
+  "test_SM2_AS_our.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_SM2_AS_our.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
